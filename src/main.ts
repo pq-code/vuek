e1bf88fc51183aae52ab51121c7c15a8fe917091
pq <784312297@qq.com>
2023-02-14 18:04:19 +0800

    存档

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import router from './router';
 
-import ElementPlus from "element-plus";
-import "element-plus/theme-chalk/index.css";
+import ElementPlus from 'element-plus';
+import 'element-plus/theme-chalk/index.css';
 
-import "./assets/styles/main.less"; // 项目样式
-import "./assets/styles/main.css";
-import "./assets/iconfont/iconfont.css";
+import './assets/styles/main.less'; // 项目样式
+import './assets/styles/main.css';
+import './assets/iconfont/iconfont.css';
 
 const app = createApp(App);
 
@@ -17,4 +17,4 @@ app.use(createPinia());
 app.use(router);
 app.use(ElementPlus);
 
-app.mount("#app");
+app.mount('#app');
